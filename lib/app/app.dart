7e86be61ff7b7ff0fc859b868a93c6da2d042084
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-11 18:59:27 +0200

    third commit

---
+++ lib/app/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Shopify extends StatelessWidget {
+  const Shopify({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold());
+  }
+}
