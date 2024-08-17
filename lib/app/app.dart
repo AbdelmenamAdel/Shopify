be790567539bb18e79336ff0010f7a7adbdbe0bb
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-17 20:20:11 +0300

    connectivity check

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,112 +1,24 @@
-import 'dart:async';
-import 'dart:developer';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:shopify/core/common/screens/no_network_view.dart';
+import 'package:shopify/core/common/screens/shopify.dart';
+import '../core/app/connectivity_controller.dart';
 
-class Shopify extends StatelessWidget {
-  const Shopify({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(
-        title: "connectivity plus",
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<ConnectivityResult> _connectionStatus = [ConnectivityResult.none];
-  final Connectivity _connectivity = Connectivity();
-  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
-
-  @override
-  void initState() {
-    super.initState();
-    initConnectivity();
-
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-  }
-
-  @override
-  void dispose() {
-    _connectivitySubscription.cancel();
-    super.dispose();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initConnectivity() async {
-    late List<ConnectivityResult> result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      result = await _connectivity.checkConnectivity();
-    } on PlatformException catch (e) {
-      log('Couldn\'t check connectivity status', error: e);
-      return;
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) {
-      return Future.value(null);
-    }
-
-    return _updateConnectionStatus(result);
-  }
-
-  Future<void> _updateConnectionStatus(List<ConnectivityResult> result) async {
-    setState(() {
-      _connectionStatus = result;
-    });
-    // ignore: avoid_print
-    log('Connectivity changed: $_connectionStatus');
-  }
+class ConnectivityCheck extends StatelessWidget {
+  const ConnectivityCheck({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Connectivity Plus Example'),
-        elevation: 4,
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Spacer(flex: 2),
-          Text(
-            'Active connection types:',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          const Spacer(),
-          ListView(
-            shrinkWrap: true,
-            children: List.generate(
-                _connectionStatus.length,
-                (index) => Center(
-                      child: Text(
-                        _connectionStatus[index].toString(),
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                    )),
-          ),
-          const Spacer(flex: 2),
-        ],
-      ),
+    ConnectivityController connectivityController = ConnectivityController();
+    connectivityController.init();
+    return ValueListenableBuilder(
+      valueListenable: connectivityController.isConnected,
+      builder: (context, value, child) {
+        if (value) {
+          return const Shopify();
+        } else {
+          return const NoNetworkView();
+        }
+      },
     );
   }
 }
