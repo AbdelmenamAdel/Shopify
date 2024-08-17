3adc55debcc4e141b02c01276b6015996f6d69a3
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-17 20:20:44 +0300

    no network view add material app

--- lib/core/common/screens/no_network_view.dart
+++ lib/core/common/screens/no_network_view.dart
@@ -6,18 +6,20 @@ class NoNetworkView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: ColoredBox(
-          color: Colors.white,
-          child: SafeArea(
-            child: Scaffold(
-              body: Container(
-                constraints: const BoxConstraints.expand(),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(Images.noNetwork),
-                    fit: BoxFit.fill,
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: ColoredBox(
+            color: Colors.white,
+            child: SafeArea(
+              child: Scaffold(
+                body: Container(
+                  constraints: const BoxConstraints.expand(),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(Images.noNetwork),
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
               ),
