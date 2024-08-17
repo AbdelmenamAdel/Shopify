7e641e294384e39b463b46d99aba163113a23a8e
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-17 20:21:00 +0300

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:shopify/app/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Shopify());
+    await tester.pumpWidget(const ConnectivityCheck());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
