e781eff8248960865441e180615ac871f9f0a354
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-18 16:19:54 +0300

    shopify file

--- lib/core/common/screens/shopify.dart
+++ lib/core/common/screens/shopify.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/language/app_localizations_setup.dart';
+import 'package:shopify/core/routes/app_routes.dart';
+import 'package:shopify/core/styles/theme/app_theme.dart';
 
 class Shopify extends StatelessWidget {
   const Shopify({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: GestureDetector(
+        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          locale: const Locale('en', 'US'),
+          supportedLocales: AppLocalizationsSetup.supportedLocales,
+          localizationsDelegates: AppLocalizationsSetup.localizationsDelegates,
+          localeResolutionCallback:
+              AppLocalizationsSetup.localeResolutionCallback,
+          theme: themeDark(),
+          onGenerateRoute: AppRoutes.onGenerateRoute,
+          initialRoute: AppRoutes.login,
+        ),
+      ),
     );
   }
 }
