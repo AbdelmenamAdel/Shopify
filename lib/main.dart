0f9172952a818df7d03f5789cdb4cbe3f1f64000
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-19 18:52:39 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopify/app/app.dart';
+import 'package:shopify/core/app/bloc_observer.dart';
 import 'package:shopify/core/app/env.variables.dart';
+import 'package:shopify/core/dependancy_injection/injection_container.dart';
+import 'package:shopify/core/services/shared_pref/shared_pref.dart';
 import 'package:shopify/core/widgets/error_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   flutterErrorWidget();
   await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
-  Firebase.initializeApp();
-  runApp(const ConnectivityCheck());
+  await Firebase.initializeApp();
+  await SharedPref().instantiatePreferences();
+  await setupInjector();
+  Bloc.observer = AppBlocObserver();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const ConnectivityCheck());
+  });
 }
