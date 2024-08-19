b139bc8f830b8dbbdda8698b94bceb9efb43cc66
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-19 18:57:36 +0300

    login button and text form

---
+++ lib/features/auth/presentation/widgets/login/login_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/animations/animate_do.dart';
+import 'package:shopify/core/common/widgets/custom_linear_button.dart';
+import 'package:shopify/core/common/widgets/text_app.dart';
+import 'package:shopify/core/extensions/context_extension.dart';
+import 'package:shopify/core/language/lang_keys.dart';
+import 'package:shopify/core/styles/fonts/font_weight_helper.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInRight(
+      duration: 600,
+      child: CustomLinearButton(
+        onPressed: () {
+          // _validateThenDoLogin(context);
+        },
+        height: 50.h,
+        width: MediaQuery.of(context).size.width,
+        child: TextApp(
+          text: context.translate(LangKeys.login),
+          theme: context.textStyle.copyWith(
+            fontSize: 18.sp,
+            fontWeight: FontWeightHelper.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  // void _validateThenDoLogin(BuildContext context) {
+  //   if (context.read<AuthBloc>().formKey.currentState!.validate()) {
+  //     context.read<AuthBloc>().add(const AuthEvent.login());
+  //   }
+  // }
+}
