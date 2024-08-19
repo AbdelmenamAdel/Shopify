4223c710274ddc4fbf5d8b49b81a07803a3898c1
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-19 18:55:07 +0300

    app theme

--- lib/core/styles/theme/app_theme.dart
+++ lib/core/styles/theme/app_theme.dart
@@ -2,20 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:shopify/core/styles/colors/colors_dark.dart';
 import 'package:shopify/core/styles/colors/colors_light.dart';
 import 'package:shopify/core/styles/fonts/font_family_helper.dart';
-
-import 'assets_extension.dart';
-import 'color_extension.dart';
+import 'package:shopify/core/styles/theme/assets_extension.dart';
+import 'package:shopify/core/styles/theme/color_extension.dart';
 
 ThemeData themeDark() {
   return ThemeData(
     scaffoldBackgroundColor: ColorsDark.mainColor,
     extensions: const <ThemeExtension<dynamic>>[MyColors.dark, MyAssets.dark],
     useMaterial3: true,
-    textTheme: TextTheme(
+    textTheme: const TextTheme(
       displaySmall: TextStyle(
         fontSize: 14,
         color: ColorsDark.white,
-        fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
+        // fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
       ),
     ),
   );
@@ -26,11 +25,11 @@ ThemeData themeLight() {
     scaffoldBackgroundColor: ColorsLight.mainColor,
     extensions: const <ThemeExtension<dynamic>>[MyColors.light, MyAssets.light],
     useMaterial3: true,
-    textTheme: TextTheme(
+    textTheme: const TextTheme(
       displaySmall: TextStyle(
         fontSize: 14,
         color: ColorsLight.black,
-        fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
+        // fontFamily: FontFamilyHelper.geLocalozedFontFamily(),
       ),
     ),
   );
