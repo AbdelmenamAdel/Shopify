270bc24f976fe1915f71b2c7385eb04325713b3e
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-19 18:53:27 +0300

    app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shopify/core/app/connectivity_controller.dart';
 import 'package:shopify/core/common/screens/no_network_view.dart';
 import 'package:shopify/core/common/screens/shopify.dart';
-import '../core/app/connectivity_controller.dart';
 
 class ConnectivityCheck extends StatelessWidget {
   const ConnectivityCheck({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ConnectivityController connectivityController = ConnectivityController();
-    connectivityController.init();
+    ConnectivityController.instance.init();
     return ValueListenableBuilder(
-      valueListenable: connectivityController.isConnected,
+      valueListenable: ConnectivityController.instance.isConnected,
       builder: (context, value, child) {
         if (value) {
           return const Shopify();
