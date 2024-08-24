d7af8fc3f731b6958957ec38bea46be2ecf8b945
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-24 08:22:54 +0300

    login text form

--- lib/features/auth/presentation/widgets/login/login_text_form.dart
+++ lib/features/auth/presentation/widgets/login/login_text_form.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopify/core/common/animations/animate_do.dart';
 import 'package:shopify/core/common/widgets/custom_text_field.dart';
