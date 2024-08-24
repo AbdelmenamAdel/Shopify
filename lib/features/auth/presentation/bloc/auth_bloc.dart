5bd46e7ac72aad1a9c599e2d650cde9d98619efa
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-24 08:24:44 +0300

    add auth bloc login method and do login successfully

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,12 +1,55 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shopify/core/services/shared_pref/pref_keys.dart';
+import 'package:shopify/core/services/shared_pref/shared_pref.dart';
+import 'package:shopify/features/auth/data/models/login/login_request_body.dart';
+import 'package:shopify/features/auth/data/repos/auth_repo.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(const _Initial()) {
-    on<AuthEvent>((event, emit) {});
+  AuthBloc(this._authRepos) : super(const _Initial()) {
+    on<LoginEvent>(_login);
+  }
+  final AuthRepos _authRepos;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  FutureOr<void> _login(LoginEvent event, Emitter<AuthState> emit) async {
+    emit(const AuthState.loading());
+    final result = await _authRepos.login(
+      LoginRequestBody(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      ),
+    );
+    await result.when(
+      success: (loginData) async {
+        final token = loginData.data.login.accessToken ?? '';
+        log(token);
+        // ! save token
+        await SharedPref().setString(
+          PrefKeys.accessToken,
+          token,
+        );
+        // ! get user role
+        final user = await _authRepos.userRole(
+          token,
+        );
+        await SharedPref().setInt(
+          PrefKeys.userId,
+          user.userId ?? 0,
+        );
+        emit(AuthState.success(userRole: user.userRole ?? ''));
+      },
+      failure: (error) {
+        emit(AuthState.error(error: error));
+      },
+    );
   }
 }
