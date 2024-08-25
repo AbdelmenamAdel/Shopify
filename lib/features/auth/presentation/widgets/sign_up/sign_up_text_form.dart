bbf72cf32505f5d0f4983abc6322ca37c488e05c
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-26 02:43:20 +0300

    add bloc to form fields

--- lib/features/auth/presentation/widgets/sign_up/sign_up_text_form.dart
+++ lib/features/auth/presentation/widgets/sign_up/sign_up_text_form.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopify/core/common/animations/animate_do.dart';
 import 'package:shopify/core/common/widgets/custom_text_field.dart';
 import 'package:shopify/core/extensions/context_extension.dart';
 import 'package:shopify/core/language/lang_keys.dart';
 import 'package:shopify/core/utils/app_regex.dart';
+import 'package:shopify/features/auth/presentation/bloc/auth_bloc.dart';
 
 class SignUpTextForm extends StatefulWidget {
   const SignUpTextForm({super.key});
@@ -15,18 +17,32 @@ class SignUpTextForm extends StatefulWidget {
 
 class _SignUpTextFormState extends State<SignUpTextForm> {
   bool isShowPassword = true;
+  late AuthBloc _bloc;
+  @override
+  void initState() {
+    _bloc = context.read<AuthBloc>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _bloc.emailController.dispose();
+    _bloc.passwordController.dispose();
+    _bloc.nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      // key: _bloc.formKey,
+      key: _bloc.formKey,
       child: Column(
         children: [
           //name
           CustomFadeInRight(
             duration: 200,
             child: CustomTextField(
-              controller: TextEditingController(),
+              controller: _bloc.nameController,
               hintText: context.translate(LangKeys.fullName),
               keyboardType: TextInputType.name,
               validator: (value) {
@@ -37,30 +53,28 @@ class _SignUpTextFormState extends State<SignUpTextForm> {
               },
             ),
           ),
-          SizedBox(
-            height: 20.h,
-          ),
+          SizedBox(height: 10.h),
           // email
           CustomFadeInRight(
             duration: 200,
             child: CustomTextField(
-              controller: TextEditingController(),
+              controller: _bloc.emailController,
               hintText: context.translate(LangKeys.email),
               keyboardType: TextInputType.emailAddress,
               validator: (value) {
-                if (!AppRegex.isEmailValid('_bloc.emailController.text')) {
+                if (!AppRegex.isEmailValid(_bloc.emailController.text)) {
                   return context.translate(LangKeys.validEmail);
                 }
                 return null;
               },
             ),
           ),
-          SizedBox(height: 20.h),
+          SizedBox(height: 10.h),
           //Password
           CustomFadeInRight(
             duration: 200,
             child: CustomTextField(
-              controller: TextEditingController(),
+              controller: _bloc.passwordController,
               hintText: context.translate(LangKeys.password),
               keyboardType: TextInputType.visiblePassword,
               obscureText: isShowPassword,
