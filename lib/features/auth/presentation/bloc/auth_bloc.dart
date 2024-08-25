08ce3fcc5f7a2a0b649f2e705f0d603db2445d3a
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-26 02:40:55 +0300

    add sign up to bloc

--- lib/features/auth/presentation/bloc/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,29 +1,32 @@
 import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:shopify/core/services/shared_pref/pref_keys.dart';
 import 'package:shopify/core/services/shared_pref/shared_pref.dart';
 import 'package:shopify/features/auth/data/models/login/login_request_body.dart';
+import 'package:shopify/features/auth/data/models/signup/sign_up_request_body.dart';
 import 'package:shopify/features/auth/data/repos/auth_repo.dart';
-
 part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc(this._authRepos) : super(const _Initial()) {
+  AuthBloc(this._repo) : super(const _Initial()) {
     on<LoginEvent>(_login);
+    on<SignUpEvent>(_signUp);
   }
-  final AuthRepos _authRepos;
+  final AuthRepos _repo;
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-  FutureOr<void> _login(LoginEvent event, Emitter<AuthState> emit) async {
+  TextEditingController nameController = TextEditingController();
+  FutureOr<void> _login(
+    LoginEvent event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(const AuthState.loading());
-    final result = await _authRepos.login(
+    final result = await _repo.login(
       LoginRequestBody(
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
@@ -38,7 +41,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           token,
         );
         // ! get user role
-        final user = await _authRepos.userRole(
+        final user = await _repo.userRole(
           token,
         );
         await SharedPref().setInt(
@@ -50,6 +53,29 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           user.userRole ?? '',
         );
         emit(AuthState.success(userRole: user.userRole ?? ''));
+      },
+      failure: (error) {
+        emit(AuthState.error(error: error));
+      },
+    );
+  }
+
+  FutureOr<void> _signUp(
+    SignUpEvent event,
+    Emitter<AuthState> emit,
+  ) async {
+    emit(const AuthState.loading());
+    final result = await _repo.signUp(
+      SignUpRequestBody(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+        name: nameController.text.trim(),
+        avatar: event.imageUrl,
+      ),
+    );
+    result.when(
+      success: (signUpData) {
+        add(const AuthEvent.login());
       },
       failure: (error) {
         emit(AuthState.error(error: error));
