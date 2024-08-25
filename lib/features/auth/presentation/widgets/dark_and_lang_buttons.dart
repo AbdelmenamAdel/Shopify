27f42ac2f670568ce8e8211225ff1ade6b762565
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-26 02:42:33 +0300

    updates in lang and color

--- lib/features/auth/presentation/widgets/dark_and_lang_buttons.dart
+++ lib/features/auth/presentation/widgets/dark_and_lang_buttons.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:shopify/app/app_cubit/app_cubit.dart';
+import 'package:shopify/core/app/app_cubit/app_cubit.dart';
 import 'package:shopify/core/common/animations/animate_do.dart';
 import 'package:shopify/core/common/widgets/custom_linear_button.dart';
 import 'package:shopify/core/common/widgets/text_app.dart';
