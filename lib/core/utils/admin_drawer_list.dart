c0936b26a95368635980e4c15dfe077aba437038
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-27 02:27:26 +0300

    admin drawer list

---
+++ lib/core/utils/admin_drawer_list.dart
@@ -0,0 +1,143 @@
+// List<DrawerItemModel>
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/dialogs/custom_dialogs.dart';
+import 'package:shopify/core/common/widgets/text_app.dart';
+import 'package:shopify/core/extensions/context_extension.dart';
+import 'package:shopify/core/styles/fonts/font_family_helper.dart';
+import 'package:shopify/core/styles/fonts/font_weight_helper.dart';
+import 'package:shopify/features/admin/categories/presentation/views/add_categories_view.dart';
+import 'package:shopify/features/admin/dashboard/presentation/views/dashboard_view.dart';
+import 'package:shopify/features/admin/notifications/presentation/views/push_notifications_view.dart';
+import 'package:shopify/features/admin/products/presentation/views/add_products_view.dart';
+import 'package:shopify/features/admin/users/presentation/views/users_view.dart';
+
+List<DrawerItemModel> adminDrawerList(BuildContext context) {
+  return <DrawerItemModel>[
+    //DashBoard
+    DrawerItemModel(
+      icon: const Icon(
+        Icons.dashboard,
+        color: Colors.white,
+      ),
+      title: TextApp(
+        text: 'DashBoard',
+        theme: context.textStyle.copyWith(
+          color: Colors.white,
+          fontSize: 17.sp,
+          fontFamily: FontFamilyHelper.poppinsEnglish,
+          fontWeight: FontWeightHelper.bold,
+        ),
+      ),
+      page: const DashboardView(),
+    ),
+    //Categories
+    DrawerItemModel(
+      icon: const Icon(Icons.category_outlined, color: Colors.white),
+      title: TextApp(
+        text: 'Categories',
+        theme: context.textStyle.copyWith(
+          color: Colors.white,
+          fontSize: 17.sp,
+          fontFamily: FontFamilyHelper.poppinsEnglish,
+          fontWeight: FontWeightHelper.bold,
+        ),
+      ),
+      page: const AddCategoriesView(),
+    ),
+    //Product
+    DrawerItemModel(
+      icon: const Icon(
+        Icons.production_quantity_limits,
+        color: Colors.white,
+      ),
+      title: TextApp(
+        text: 'Products',
+        theme: context.textStyle.copyWith(
+          color: Colors.white,
+          fontSize: 17.sp,
+          fontFamily: FontFamilyHelper.poppinsEnglish,
+          fontWeight: FontWeightHelper.bold,
+        ),
+      ),
+      page: const AddProductsView(),
+    ),
+    //Users
+    DrawerItemModel(
+      icon: const Icon(
+        Icons.people_alt_rounded,
+        color: Colors.white,
+      ),
+      title: TextApp(
+        text: 'Users',
+        theme: context.textStyle.copyWith(
+          color: Colors.white,
+          fontSize: 17.sp,
+          fontFamily: FontFamilyHelper.poppinsEnglish,
+          fontWeight: FontWeightHelper.bold,
+        ),
+      ),
+      page: const UsersView(),
+    ),
+    //Notifications
+    DrawerItemModel(
+      icon: const Icon(
+        Icons.notifications_active,
+        color: Colors.white,
+      ),
+      title: TextApp(
+        text: 'Notifications',
+        theme: context.textStyle.copyWith(
+          color: Colors.white,
+          fontSize: 17.sp,
+          fontFamily: FontFamilyHelper.poppinsEnglish,
+          fontWeight: FontWeightHelper.bold,
+        ),
+      ),
+      page: const PushNotificationsView(),
+    ),
+    //LogOut
+    DrawerItemModel(
+      icon: const Icon(
+        Icons.exit_to_app,
+        color: Colors.white,
+      ),
+      title: GestureDetector(
+        onTap: () {
+          CustomDialog.twoButtonDialog(
+            context: context,
+            textBody: 'Do you want log out?',
+            textButton1: 'Yes',
+            textButton2: 'No',
+            isLoading: false,
+            onPressed: () async {
+              // await AppLogout().logout();
+            },
+          );
+        },
+        child: const Text(
+          'Logout',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeightHelper.bold,
+            fontSize: 17,
+          ),
+        ),
+      ),
+      page: const UsersView(),
+    ),
+  ];
+}
+
+class DrawerItemModel {
+  DrawerItemModel({
+    required this.icon,
+    required this.title,
+    required this.page,
+  });
+
+  final Icon icon;
+  final Widget title;
+  final Widget page;
+}
