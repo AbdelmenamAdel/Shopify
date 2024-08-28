91c8e0cd96be205f0f786fad95d81d38df3c9281
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-28 03:27:22 +0300

    update sl

--- lib/core/dependancy_injection/injection_container.dart
+++ lib/core/dependancy_injection/injection_container.dart
@@ -6,6 +6,27 @@ import 'package:shopify/core/app/upload_image/data_source/upload_image_data_sour
 import 'package:shopify/core/app/upload_image/repo/upload_image_repo.dart';
 import 'package:shopify/core/services/graphql/api_service.dart';
 import 'package:shopify/core/services/graphql/dio_factory.dart';
+import 'package:shopify/features/admin/categories/data/data_source/categories_admin_data_sourse.dart';
+import 'package:shopify/features/admin/categories/data/repos/categoreis_admin_repos.dart';
+import 'package:shopify/features/admin/categories/presentation/bloc/create_category/create_category_bloc.dart';
+import 'package:shopify/features/admin/categories/presentation/bloc/delete_category/delete_category_bloc.dart';
+import 'package:shopify/features/admin/categories/presentation/bloc/get_all_admin_categories/get_all_admin_categories_bloc.dart';
+import 'package:shopify/features/admin/categories/presentation/bloc/update_category/update_category_bloc.dart';
+import 'package:shopify/features/admin/dashboard/data/bloc/categories_count/categories_count_bloc.dart';
+import 'package:shopify/features/admin/dashboard/data/bloc/products_count/products_count_bloc.dart';
+import 'package:shopify/features/admin/dashboard/data/bloc/users_count/users_count_bloc.dart';
+import 'package:shopify/features/admin/dashboard/data/data_source/dashboard_data_sourse.dart';
+import 'package:shopify/features/admin/dashboard/data/repos/dashboard_repo.dart';
+import 'package:shopify/features/admin/products/data/data_source/products_admin_data_source.dart';
+import 'package:shopify/features/admin/products/data/repos/products_admin_repo.dart';
+import 'package:shopify/features/admin/products/presentation/bloc/create_product/create_prodcut_bloc.dart';
+import 'package:shopify/features/admin/products/presentation/bloc/delete_product/delete_product_bloc.dart';
+import 'package:shopify/features/admin/products/presentation/bloc/get_all_admin_products/get_all_admin_products_bloc.dart';
+import 'package:shopify/features/admin/products/presentation/bloc/update_product/update_product_bloc.dart';
+import 'package:shopify/features/admin/users/data/data_source/users_data_source.dart';
+import 'package:shopify/features/admin/users/data/repos/users_repo.dart';
+import 'package:shopify/features/admin/users/presentation/bloc/delete_user/delete_user_bloc.dart';
+import 'package:shopify/features/admin/users/presentation/bloc/get_all_users/get_all_users_bloc.dart';
 import 'package:shopify/features/auth/data/data_source/auth_data_source.dart';
 import 'package:shopify/features/auth/data/repos/auth_repo.dart';
 import 'package:shopify/features/auth/presentation/bloc/auth_bloc.dart';
@@ -15,10 +36,10 @@ final sl = GetIt.instance;
 Future<void> setupInjector() async {
   await _initCore();
   await _initAuth();
-  // await _initDashBoard();
-  // await _initCategoriesAdmin();
-  // await _initProductsAdmin();
-  // await _initUsersAdmin();
+  await _initDashBoard();
+  await _initCategoriesAdmin();
+  await _initProductsAdmin();
+  await _initUsersAdmin();
   // await _initAddNotification();
   // await _initMain();
   // await _initProfile();
@@ -51,42 +72,42 @@ Future<void> _initAuth() async {
     ..registerLazySingleton(() => AuthDataSource(sl()));
 }
 
-// Future<void> _initDashBoard() async {
-//   sl
-//     ..registerLazySingleton(() => DashBoardDataSource(sl()))
-//     ..registerLazySingleton(() => DashBoardRepo(sl()))
-//     ..registerFactory(() => ProductsNumberBloc(sl()))
-//     ..registerFactory(() => CategoriesNumberBloc(sl()))
-//     ..registerFactory(() => UsersNumberBloc(sl()));
-// }
+Future<void> _initDashBoard() async {
+  sl
+    ..registerLazySingleton(() => DashBoardDataSourse(sl()))
+    ..registerLazySingleton(() => DashBoardRepo(sl()))
+    ..registerFactory(() => ProductsCountBloc(sl()))
+    ..registerFactory(() => CategoriesCountBloc(sl()))
+    ..registerFactory(() => UsersCountBloc(sl()));
+}
 
-// Future<void> _initCategoriesAdmin() async {
-//   sl
-//     ..registerLazySingleton(() => CategoreisAdminRepo(sl()))
-//     ..registerLazySingleton(() => CategoriesAdminDataSource(sl()))
-//     ..registerFactory(() => GetAllAdminCategoriesBloc(sl()))
-//     ..registerFactory(() => CreateCategoryBloc(sl()))
-//     ..registerFactory(() => DeleteCategoryBloc(sl()))
-//     ..registerFactory(() => UpdateCategoryBloc(sl()));
-// }
+Future<void> _initCategoriesAdmin() async {
+  sl
+    ..registerLazySingleton(() => CategoreisAdminRepo(sl()))
+    ..registerLazySingleton(() => CategoriesAdminDataSource(sl()))
+    ..registerFactory(() => GetAllAdminCategoriesBloc(sl()))
+    ..registerFactory(() => CreateCategoryBloc(sl()))
+    ..registerFactory(() => DeleteCategoryBloc(sl()))
+    ..registerFactory(() => UpdateCategoryBloc(sl()));
+}
 
-// Future<void> _initProductsAdmin() async {
-//   sl
-//     ..registerLazySingleton(() => ProductsAdminRepo(sl()))
-//     ..registerLazySingleton(() => ProductsAdminDataSource(sl()))
-//     ..registerFactory(() => GetAllAdminProductBloc(sl()))
-//     ..registerFactory(() => CreateProdcutBloc(sl()))
-//     ..registerFactory(() => DeleteProductBloc(sl()))
-//     ..registerFactory(() => UpdateProductBloc(sl()));
-// }
+Future<void> _initProductsAdmin() async {
+  sl
+    ..registerLazySingleton(() => ProductsAdminRepo(sl()))
+    ..registerLazySingleton(() => ProductsAdminDataSource(sl()))
+    ..registerFactory(() => GetAllAdminProductsBloc(sl()))
+    ..registerFactory(() => CreateProdcutBloc(sl()))
+    ..registerFactory(() => DeleteProductBloc(sl()))
+    ..registerFactory(() => UpdateProductBloc(sl()));
+}
 
-// Future<void> _initUsersAdmin() async {
-//   sl
-//     ..registerLazySingleton(() => UsersRepo(sl()))
-//     ..registerLazySingleton(() => UserDataSource(sl()))
-//     ..registerFactory(() => GetAllUsersBloc(sl()))
-//     ..registerFactory(() => DeleteUserBloc(sl()));
-// }
+Future<void> _initUsersAdmin() async {
+  sl
+    ..registerLazySingleton(() => UsersRepo(sl()))
+    ..registerLazySingleton(() => UsersDataSource(sl()))
+    ..registerFactory(() => GetAllUsersBloc(sl()))
+    ..registerFactory(() => DeleteUserBloc(sl()));
+}
 
 // Future<void> _initAddNotification() async {
 //   sl
