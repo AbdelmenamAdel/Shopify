079738c425e46b2477bf79bb86da1939c9d9b78b
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-08-28 03:28:27 +0300

    products queries

--- lib/core/services/graphql/admin/products_queries.dart
+++ lib/core/services/graphql/admin/products_queries.dart
@@ -1,104 +1,107 @@
-// class ProductsQueries {
-//   factory ProductsQueries() {
-//     return _instance;
-//   }
-//   const ProductsQueries._();
+import 'package:shopify/features/admin/products/data/models/create_product_request_body.dart';
+import 'package:shopify/features/admin/products/data/models/update_product_request_body.dart';
 
-//   static const ProductsQueries _instance = ProductsQueries._();
+class ProductsQueries {
+  factory ProductsQueries() {
+    return _instance;
+  }
+  const ProductsQueries._();
 
-//   //Product get all
-//   Map<String, dynamic> getAllProductsMapQuery() {
-//     return {
-//       'query': '''
-//             {
-//             products{
-//               id
-//               title
-//               price
-//               images
-//               description
-//               category{
-//                 id
-//                 name
-//                 }
-//             }
-//           }
-//       ''',
-//     };
-//   }
+  static const ProductsQueries _instance = ProductsQueries._();
 
-//   //Create Product
-//   Map<String, dynamic> createProductMap({
-//     required CreateProductRequestBody body,
-//   }) {
-//     return {
-//       'query': r'''
-//           mutation CreateProduct($title: String!, $price: Float!, $description:String!, $categoryId: Float!,$imagesList:[String!]!  ) {
-//               addProduct(
-//                 data: {
-//                   title: $title,
-//                   price: $price
-//                   description: $description
-//                   categoryId: $categoryId
-//                   images: $imagesList
-//                 }
-//               ) {
-//                 title
-//               }
-//             }
-//         ''',
-//       'variables': {
-//         'title': body.title,
-//         'price': body.price,
-//         'description': body.description,
-//         'categoryId': body.categoryId,
-//         'imagesList': body.imageList,
-//       },
-//     };
-//   }
+  //Product get all
+  Map<String, dynamic> getAllProductsMapQuery() {
+    return {
+      'query': '''
+            {
+            products{
+              id
+              title
+              price
+              images
+              description
+              category{
+                id
+                name
+                }
+            }
+          }
+      ''',
+    };
+  }
 
-//   Map<String, dynamic> deleteMapQuery({
-//     required String productId,
-//   }) {
-//     return {
-//       'query': r'''
-//             mutation DeleteCategory($productId: ID!) {
-//               deleteProduct(id: $productId)
-//             }
-//       ''',
-//       'variables': {
-//         'productId': productId,
-//       },
-//     };
-//   }
+  //Create Product
+  Map<String, dynamic> createProductMapQuery({
+    required CreateProductRequestBody body,
+  }) {
+    return {
+      'query': r'''
+          mutation CreateProduct($title: String!, $price: Float!, $description:String!, $categoryId: Float!,$imagesList:[String!]!  ) {
+              addProduct(
+                data: {
+                  title: $title,
+                  price: $price
+                  description: $description
+                  categoryId: $categoryId
+                  images: $imagesList
+                }
+              ) {
+                title
+              }
+            }
+        ''',
+      'variables': {
+        'title': body.title,
+        'price': body.price,
+        'description': body.description,
+        'categoryId': body.categoryId,
+        'imagesList': body.imageList,
+      },
+    };
+  }
 
-//   //Update Product
-//   Map<String, dynamic> updateProductMap({
-//     required UpdateProductRequestBody body,
-//   }) {
-//     return {
-//       'query': r'''
-//             mutation UpdateProduct($productId: ID!, $title: String!,$description: String!,$imageList:[String!]!,$price: Float!,$categoryId: Float! ) {
-//           updateProduct(id: $productId, 
-//           changes: { 
-//             title: $title,
-//             categoryId: $categoryId,
-//             images:$imageList,
-//             description: $description,
-//             price:$price
-//             }) {
-//             title
-//           }
-//         }
-//         ''',
-//       'variables': {
-//         'productId': body.productId,
-//         'title': body.title,
-//         'description': body.description,
-//         'imageList': body.imageList,
-//         'categoryId': body.categoryId,
-//         'price': body.price,
-//       },
-//     };
-//   }
-// }
+  Map<String, dynamic> deleteProductMapQuery({
+    required String productId,
+  }) {
+    return {
+      'query': r'''
+            mutation DeleteCategory($productId: ID!) {
+              deleteProduct(id: $productId)
+            }
+      ''',
+      'variables': {
+        'productId': productId,
+      },
+    };
+  }
+
+  //Update Product
+  Map<String, dynamic> updateProductMapQuery({
+    required UpdateProductRequestBody body,
+  }) {
+    return {
+      'query': r'''
+            mutation UpdateProduct($productId: ID!, $title: String!,$description: String!,$imageList:[String!]!,$price: Float!,$categoryId: Float! ) {
+          updateProduct(id: $productId,
+          changes: {
+            title: $title,
+            categoryId: $categoryId,
+            images:$imageList,
+            description: $description,
+            price:$price
+            }) {
+            title
+          }
+        }
+        ''',
+      'variables': {
+        'productId': body.productId,
+        'title': body.title,
+        'description': body.description,
+        'imageList': body.imageList,
+        'categoryId': body.categoryId,
+        'price': body.price,
+      },
+    };
+  }
+}
