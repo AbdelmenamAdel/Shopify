883cca53b043a8da605c7757742e725329ca7d95
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-10-16 07:38:19 +0300

    app log out

---
+++ lib/core/utils/app_logout.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/widgets.dart';
+// import 'package:shopify/core/routes/app_routes.dart';
+// import 'package:shopify/core/services/shared_pref/pref_keys.dart';
+// import 'package:shopify/core/services/shared_pref/shared_pref.dart';
+
+// class AppLogout {
+//   factory AppLogout() {
+//     return _instance;
+//   }
+
+//   AppLogout._();
+
+//   static final AppLogout _instance = AppLogout._();
+
+//   Future<void> logout() async {
+//     final context = sl<GlobalKey<NavigatorState>>().currentState!.context;
+//     await SharedPref().removePreference(PrefKeys.accessToken);
+//     await SharedPref().removePreference(PrefKeys.userId);
+//     await SharedPref().removePreference(PrefKeys.userRole);
+//     await HiveDatabase().clearAllBox();
+//     if (!context.mounted) return;
+//     await context.pushNamedAndRemoveUntil(AppRoutes.login);
+//   }
+// }
