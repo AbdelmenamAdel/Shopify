ad3ed17a61e851df27c0915c259101f9df382e85
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-10-25 10:28:48 +0300

    add the row notification on and off

--- lib/features/customer/customer_home_view.dart
+++ lib/features/customer/customer_home_view.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/widgets/text_app.dart';
+import 'package:shopify/core/extensions/context_extension.dart';
+import 'package:shopify/core/language/lang_keys.dart';
+import 'package:shopify/core/services/push_notification/firebase_cloud_messaging.dart';
+import 'package:shopify/core/styles/fonts/font_weight_helper.dart';
+
 class CustomerHomeView extends StatelessWidget {
   const CustomerHomeView({super.key});
 
@@ -10,6 +17,51 @@ class CustomerHomeView extends StatelessWidget {
         centerTitle: true,
         title: const Text('Customer Home'),
       ),
+      body: const Center(
+        child: NotificationsSettings(),
+      ),
+    );
+  }
+}
+
+class NotificationsSettings extends StatelessWidget {
+  const NotificationsSettings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.notifications_active_outlined,
+          color: context.textStyle.color,
+        ),
+        SizedBox(width: 10.w),
+        TextApp(
+          text: context.translate(LangKeys.notifications),
+          theme: context.textStyle.copyWith(
+            fontSize: 18.sp,
+            fontWeight: FontWeightHelper.regular,
+          ),
+        ),
+        const Spacer(),
+        ValueListenableBuilder(
+          valueListenable: FirebaseCloudMessaging().isSubscribe,
+          builder: (_, value, __) {
+            return Transform.scale(
+              scale: 1,
+              child: Switch.adaptive(
+                inactiveTrackColor: const Color(0XFF262626),
+                activeColor: Colors.green,
+                value: value,
+                onChanged: (value) {
+                  FirebaseCloudMessaging()
+                      .controllerForUserSubscription(context);
+                },
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
