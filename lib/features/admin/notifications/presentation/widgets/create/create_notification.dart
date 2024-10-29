9abcd288476cb5bd84ee6f5123fd211785b6313a
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-10-29 18:11:34 +0300

    row

---
+++ lib/features/admin/notifications/presentation/widgets/create/create_notification.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/bottom_sheet/custom_bottom_sheet.dart';
+import 'package:shopify/core/common/widgets/custom_button.dart';
+import 'package:shopify/core/common/widgets/text_app.dart';
+import 'package:shopify/core/extensions/context_extension.dart';
+import 'package:shopify/core/styles/colors/colors_dark.dart';
+import 'package:shopify/core/styles/fonts/font_family_helper.dart';
+import 'package:shopify/core/styles/fonts/font_weight_helper.dart';
+import 'package:shopify/features/admin/notifications/presentation/widgets/create/create_notification_bottom_sheet.dart';
+
+class CreateNotification extends StatelessWidget {
+  const CreateNotification({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // titile
+        TextApp(
+          text: 'Notifications',
+          theme: context.textStyle.copyWith(
+            fontSize: 18.sp,
+            fontWeight: FontWeightHelper.medium,
+            fontFamily: FontFamilyHelper.poppinsEnglish,
+          ),
+        ),
+        // bottom sheet
+        CustomButton(
+          onPressed: () {
+            CustomBottomSheet.showModalBottomSheetContainer(
+              context: context,
+              widget: const CreateNotificationBottomSheet(),
+
+              // whenComplete: () {
+              //   context.read<GetAllNotificationAdminBloc>().add(
+              //         const GetAllNotificationAdminEvent.getAllNotificion(),
+              //       );
+              // },
+            );
+          },
+          backgroundColor: ColorsDark.blueDark,
+          lastRadius: 10,
+          threeRadius: 10,
+          text: 'Add',
+          width: 90.w,
+          height: 35.h,
+        ),
+      ],
+    );
+  }
+}
