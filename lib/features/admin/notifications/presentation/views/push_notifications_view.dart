b28be874f86cf93e482b0b95551838156badb179
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-03 01:50:26 +0200

    fix get bloc error

--- lib/features/admin/notifications/presentation/views/push_notifications_view.dart
+++ lib/features/admin/notifications/presentation/views/push_notifications_view.dart
@@ -3,7 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopify/core/common/widgets/admin_app_bar.dart';
 import 'package:shopify/core/dependancy_injection/injection_container.dart';
 import 'package:shopify/core/styles/colors/colors_dark.dart';
+import 'package:shopify/features/admin/notifications/presentation/bloc/add_notification/add_notification_bloc.dart';
 import 'package:shopify/features/admin/notifications/presentation/bloc/get_all_notification_admin/get_all_notification_admin_bloc.dart';
+import 'package:shopify/features/admin/notifications/presentation/bloc/send_notification/send_notification_bloc.dart';
 import 'package:shopify/features/admin/notifications/presentation/refactors/push_notification_body.dart';
 
 class PushNotificationsView extends StatelessWidget {
@@ -11,8 +13,17 @@ class PushNotificationsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => sl<GetAllNotificationAdminBloc>(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => sl<GetAllNotificationAdminBloc>()
+            ..add(
+              const GetAllNotificationAdminEvent.getAllNotificion(),
+            ),
+        ),
+        BlocProvider(create: (context) => sl<AddNotificationBloc>()),
+        BlocProvider(create: (context) => sl<SendNotificationBloc>()),
+      ],
       child: const Scaffold(
         backgroundColor: ColorsDark.mainColor,
         appBar: AdminAppBar(
