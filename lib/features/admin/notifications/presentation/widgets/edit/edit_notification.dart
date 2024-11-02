781c151e097e0249fc63f2197c5fcf32fba0a6da
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-02 13:04:57 +0200

    edit notification icon and it's sheet

--- lib/features/admin/notifications/presentation/widgets/edit/edit_notification.dart
+++ lib/features/admin/notifications/presentation/widgets/edit/edit_notification.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopify/core/common/bottom_sheet/custom_bottom_sheet.dart';
+import 'package:shopify/features/admin/notifications/data/models/push_notification_model.dart';
+import 'package:shopify/features/admin/notifications/presentation/bloc/get_all_notification_admin/get_all_notification_admin_bloc.dart';
 import 'package:shopify/features/admin/notifications/presentation/widgets/edit/edit_notification_bottom_sheet.dart';
 
 class EditNotification extends StatelessWidget {
   const EditNotification({
-    // required this.notificationModel,
+    required this.notificationModel,
     super.key,
   });
 
-  // final AddNotificationModel notificationModel;
+  final PushNotificationModel notificationModel;
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +19,13 @@ class EditNotification extends StatelessWidget {
       onTap: () {
         CustomBottomSheet.showModalBottomSheetContainer(
           context: context,
-          widget: const EditNotificationBottomSheet(
-              // notificationModel: notificationModel,
-              ),
+          widget: EditNotificationBottomSheet(
+            notificationModel: notificationModel,
+          ),
           whenComplete: () {
-            // context.read<GetAllNotificationAdminBloc>().add(
-            //       const GetAllNotificationAdminEvent.getAllNotificion(),
-            //     );
+            context.read<GetAllNotificationAdminBloc>().add(
+                  const GetAllNotificationAdminEvent.getAllNotificion(),
+                );
           },
         );
       },
