3307dadeeef71cd9b5dc5c95195eacb0c8da7234
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-02 12:49:12 +0200

    add bloc to view

--- lib/features/admin/notifications/presentation/views/push_notifications_view.dart
+++ lib/features/admin/notifications/presentation/views/push_notifications_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopify/core/common/widgets/admin_app_bar.dart';
+import 'package:shopify/core/dependancy_injection/injection_container.dart';
 import 'package:shopify/core/styles/colors/colors_dark.dart';
+import 'package:shopify/features/admin/notifications/presentation/bloc/get_all_notification_admin/get_all_notification_admin_bloc.dart';
 import 'package:shopify/features/admin/notifications/presentation/refactors/push_notification_body.dart';
 
 class PushNotificationsView extends StatelessWidget {
@@ -8,14 +11,17 @@ class PushNotificationsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: ColorsDark.mainColor,
-      appBar: AdminAppBar(
-        isMain: true,
+    return BlocProvider(
+      create: (context) => sl<GetAllNotificationAdminBloc>(),
+      child: const Scaffold(
         backgroundColor: ColorsDark.mainColor,
-        title: 'Notifications',
+        appBar: AdminAppBar(
+          isMain: true,
+          backgroundColor: ColorsDark.mainColor,
+          title: 'Notifications',
+        ),
+        body: PushNotificationBody(),
       ),
-      body: PushNotificationBody(),
     );
   }
 }
