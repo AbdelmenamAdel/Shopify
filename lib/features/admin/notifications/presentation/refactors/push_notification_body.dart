ecaea346f967b689aa6ddab7208bdfdd5797a42c
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-02 13:14:06 +0200

    add logic to get all notifications

--- lib/features/admin/notifications/presentation/refactors/push_notification_body.dart
+++ lib/features/admin/notifications/presentation/refactors/push_notification_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/loading/empty_screen.dart';
+import 'package:shopify/core/common/widgets/custom_loading_indecator.dart';
+import 'package:shopify/features/admin/notifications/presentation/bloc/get_all_notification_admin/get_all_notification_admin_bloc.dart';
 import 'package:shopify/features/admin/notifications/presentation/widgets/add_notification_item.dart';
 import 'package:shopify/features/admin/notifications/presentation/widgets/create/create_notification.dart';
 
@@ -19,18 +23,38 @@ class PushNotificationBody extends StatelessWidget {
             child: CreateNotification(),
           ),
           SliverToBoxAdapter(
-            child: ListView.separated(
-              padding: EdgeInsets.symmetric(vertical: 20.h),
-              itemCount: 10,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return const AddNotificationItem(
-                    // notificationModel: notificaionList[index],
-                    // index: index,
+            child: BlocBuilder<GetAllNotificationAdminBloc,
+                GetAllNotificationAdminState>(
+              builder: (context, state) {
+                return state.when(
+                  loading: () {
+                    return Center(
+                      child: SizedBox(
+                        height: 50.h,
+                        child: const CustomLoadingIndecator(),
+                      ),
                     );
+                  },
+                  success: (notificaionList) {
+                    return ListView.separated(
+                      padding: EdgeInsets.symmetric(vertical: 20.h),
+                      itemCount: notificaionList.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return AddNotificationItem(
+                          notificationModel: notificaionList[index],
+                          index: index,
+                        );
+                      },
+                      separatorBuilder: (context, index) =>
+                          SizedBox(height: 15.h),
+                    );
+                  },
+                  empty: EmptyScreen.new,
+                  error: Text.new,
+                );
               },
-              separatorBuilder: (context, index) => SizedBox(height: 15.h),
             ),
           ),
         ],
