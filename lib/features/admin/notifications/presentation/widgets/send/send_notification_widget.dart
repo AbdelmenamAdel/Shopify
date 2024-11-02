475b36dd64ad08b5b67e608e7e2b58aa4bf0d73a
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-02 12:47:25 +0200

    send notification is done without some logic

--- lib/features/admin/notifications/presentation/widgets/send/send_notification_widget.dart
+++ lib/features/admin/notifications/presentation/widgets/send/send_notification_widget.dart
@@ -1,76 +1,78 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:shopify/core/common/toast/show_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/toast/show_toast.dart';
+import 'package:shopify/features/admin/notifications/data/models/push_notification_model.dart';
+import 'package:shopify/features/admin/notifications/presentation/bloc/send_notification/send_notification_bloc.dart';
 
-// class SendNotificationWidget extends StatelessWidget {
-//   const SendNotificationWidget({
-//     required this.notificationModel,
-//     required this.index,
-//     super.key,
-//   });
+class SendNotificationWidget extends StatelessWidget {
+  const SendNotificationWidget({
+    required this.notificationModel,
+    required this.index,
+    super.key,
+  });
 
-//   final AddNotificationModel notificationModel;
-//   final int index;
+  final PushNotificationModel notificationModel;
+  final int index;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return BlocConsumer<SendNotificationBloc, SendNotificationState>(
-//       listener: (context, state) {
-//         state.whenOrNull(
-//           success: () {
-//             ShowToast.showToastSuccessTop(
-//               message: 'Your notifications have been send successfully',
-//             );
-//           },
-//           error: (error) {
-//             ShowToast.showToastErrorTop(
-//               message: error,
-//             );
-//           },
-//         );
-//       },
-//       builder: (context, state) {
-//         return state.maybeWhen(
-//           loading: (indexId) {
-//             if (index == indexId) {
-//               return SizedBox(
-//                 height: 25.h,
-//                 width: 25.w,
-//                 child: const Center(
-//                   child: CircularProgressIndicator(
-//                     color: Colors.white,
-//                   ),
-//                 ),
-//               );
-//             }
-//             return const Icon(
-//               Icons.send_rounded,
-//               color: Colors.green,
-//               size: 25,
-//             );
-//           },
-//           orElse: () {
-//             return InkWell(
-//               onTap: () {
-//                 context.read<SendNotificationBloc>().add(
-//                       SendNotificationEvent.sendNotifications(
-//                         title: notificationModel.title,
-//                         body: notificationModel.body,
-//                         productId: notificationModel.productId,
-//                         indexId: index,
-//                       ),
-//                     );
-//               },
-//               child: const Icon(
-//                 Icons.send_rounded,
-//                 color: Colors.green,
-//                 size: 25,
-//               ),
-//             );
-//           },
-//         );
-//       },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SendNotificationBloc, SendNotificationState>(
+      listener: (context, state) {
+        state.whenOrNull(
+          success: () {
+            ShowToast.showToastSuccessTop(
+              message: 'Your notifications have been send successfully',
+            );
+          },
+          error: (error) {
+            ShowToast.showToastErrorTop(
+              message: error,
+            );
+          },
+        );
+      },
+      builder: (context, state) {
+        return state.maybeWhen(
+          loading: (indexId) {
+            if (index == indexId) {
+              return SizedBox(
+                height: 25.h,
+                width: 25.w,
+                child: const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            }
+            return const Icon(
+              Icons.send_rounded,
+              color: Colors.green,
+              size: 25,
+            );
+          },
+          orElse: () {
+            return InkWell(
+              onTap: () {
+                context.read<SendNotificationBloc>().add(
+                      SendNotificationEvent.sendNotifications(
+                        title: notificationModel.title,
+                        body: notificationModel.body,
+                        productId: notificationModel.productId,
+                        indexId: index,
+                      ),
+                    );
+              },
+              child: const Icon(
+                Icons.send_rounded,
+                color: Colors.green,
+                size: 25,
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
