8e48f043c185f19bfaf79f15deb9386adf702d48
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-02 13:05:14 +0200

    this is the icon 🙃

--- lib/features/admin/notifications/presentation/widgets/send/send_notification_widget.dart
+++ lib/features/admin/notifications/presentation/widgets/send/send_notification_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopify/core/common/toast/show_toast.dart';
+import 'package:shopify/core/dependancy_injection/injection_container.dart';
 import 'package:shopify/features/admin/notifications/data/models/push_notification_model.dart';
 import 'package:shopify/features/admin/notifications/presentation/bloc/send_notification/send_notification_bloc.dart';
 
@@ -17,62 +18,65 @@ class SendNotificationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<SendNotificationBloc, SendNotificationState>(
-      listener: (context, state) {
-        state.whenOrNull(
-          success: () {
-            ShowToast.showToastSuccessTop(
-              message: 'Your notifications have been send successfully',
-            );
-          },
-          error: (error) {
-            ShowToast.showToastErrorTop(
-              message: error,
-            );
-          },
-        );
-      },
-      builder: (context, state) {
-        return state.maybeWhen(
-          loading: (indexId) {
-            if (index == indexId) {
-              return SizedBox(
-                height: 25.h,
-                width: 25.w,
-                child: const Center(
-                  child: CircularProgressIndicator(
-                    color: Colors.white,
-                  ),
-                ),
+    return BlocProvider(
+      create: (context) => sl<SendNotificationBloc>(),
+      child: BlocConsumer<SendNotificationBloc, SendNotificationState>(
+        listener: (context, state) {
+          state.whenOrNull(
+            success: () {
+              ShowToast.showToastSuccessTop(
+                message: 'Your notifications have been send successfully',
               );
-            }
-            return const Icon(
-              Icons.send_rounded,
-              color: Colors.green,
-              size: 25,
-            );
-          },
-          orElse: () {
-            return InkWell(
-              onTap: () {
-                context.read<SendNotificationBloc>().add(
-                      SendNotificationEvent.sendNotifications(
-                        title: notificationModel.title,
-                        body: notificationModel.body,
-                        productId: notificationModel.productId,
-                        indexId: index,
-                      ),
-                    );
-              },
-              child: const Icon(
+            },
+            error: (error) {
+              ShowToast.showToastErrorTop(
+                message: error,
+              );
+            },
+          );
+        },
+        builder: (context, state) {
+          return state.maybeWhen(
+            loading: (indexId) {
+              if (index == indexId) {
+                return SizedBox(
+                  height: 25.h,
+                  width: 25.w,
+                  child: const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              }
+              return const Icon(
                 Icons.send_rounded,
                 color: Colors.green,
                 size: 25,
-              ),
-            );
-          },
-        );
-      },
+              );
+            },
+            orElse: () {
+              return InkWell(
+                onTap: () {
+                  context.read<SendNotificationBloc>().add(
+                        SendNotificationEvent.sendNotifications(
+                          title: notificationModel.title,
+                          body: notificationModel.body,
+                          productId: notificationModel.productId,
+                          indexId: index,
+                        ),
+                      );
+                },
+                child: const Icon(
+                  Icons.send_rounded,
+                  color: Colors.green,
+                  size: 25,
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
