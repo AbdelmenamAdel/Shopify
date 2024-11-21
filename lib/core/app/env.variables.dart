ebd1db363fe01d69712ca7e728af54211f64601e
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-21 18:51:21 +0200

    fix error in build developer

--- lib/core/app/env.variables.dart
+++ lib/core/app/env.variables.dart
@@ -5,6 +5,7 @@ enum EnvTypeEnum { dev, prod }
 class EnvVariable {
   EnvVariable._();
   static final EnvVariable instance = EnvVariable._();
+  String _buildDeveloper = '';
   String _envType = '';
   String _notifcationBaseUrl = '';
   Future<void> init({required EnvTypeEnum envType}) async {
@@ -17,9 +18,11 @@ class EnvVariable {
     // Consider adding error handling if ENV_TYPE is not found
     _envType = dotenv.get('ENV_TYPE');
     _notifcationBaseUrl = dotenv.get('NOTFICATION_BASEURL');
+    _buildDeveloper = dotenv.get('BUILD_DEVELOPER');
   }
 
   /// Indicates if the current environment is for development.
   bool get debugMode => _envType == 'dev';
   String get notifcationBaseUrl => _notifcationBaseUrl;
+  String get buildDeveloper => _buildDeveloper;
 }
