00a4a0971d318bfe49f35d4b2e9ae6cd416ac798
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-21 17:17:52 +0200

    show app bar only in home view

--- lib/features/customer/home/home_view.dart
+++ lib/features/customer/home/home_view.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:shopify/core/common/dialogs/custom_dialogs.dart';
+import 'package:shopify/core/utils/app_logout.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Home View',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 20,
+    return Center(
+      child: InkWell(
+        onTap: () {
+          CustomDialog.twoButtonDialog(
+            context: context,
+            textBody: 'Do you want log out?',
+            textButton1: 'Yes',
+            textButton2: 'No',
+            isLoading: false,
+            onPressed: () async {
+              await AppLogout().logout();
+            },
+          );
+        },
+        child: const Text(
+          'Home View',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
         ),
       ),
     );
--- lib/features/customer/main/presentation/refactor/home_app_bar.dart
+++ lib/features/customer/main/presentation/refactor/home_app_bar.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shopify/core/common/animations/animate_do.dart';
 import 'package:shopify/core/common/widgets/custom_linear_button.dart';
 import 'package:shopify/core/common/widgets/text_app.dart';
+import 'package:shopify/core/enums/nav_bar_enum.dart';
 import 'package:shopify/core/extensions/context_extension.dart';
 import 'package:shopify/core/language/lang_keys.dart';
 import 'package:shopify/core/styles/fonts/font_weight_helper.dart';
 import 'package:shopify/core/styles/images/app_images.dart';
+import 'package:shopify/features/customer/main/presentation/manager/cubit/main_cubit.dart';
 
 class CustomerMainAppBar extends StatelessWidget
     implements PreferredSizeWidget {
@@ -15,32 +18,42 @@ class CustomerMainAppBar extends StatelessWidget
 
   @override
   Widget build(BuildContext context) {
+    final cubit = context.read<MainCubit>();
     return AppBar(
       elevation: 0,
       automaticallyImplyLeading: false,
       backgroundColor: context.color.mainColor,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          CustomFadeInRight(
-            duration: 800,
-            child: TextApp(
-              text: context.translate(LangKeys.chooseProducts),
-              theme: context.textStyle.copyWith(
-                color: context.color.textColor,
-                fontWeight: FontWeightHelper.bold,
-                fontSize: 20.sp,
-              ),
-            ),
-          ),
-          CustomFadeInLeft(
-            duration: 800,
-            child: CustomLinearButton(
-              onPressed: () {},
-              child: Center(child: SvgPicture.asset(AppImages.search)),
-            ),
-          ),
-        ],
+      title: BlocBuilder(
+        bloc: cubit,
+        builder: (context, state) {
+          if (cubit.navBarEnum == NavBarEnum.home) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomFadeInRight(
+                  duration: 800,
+                  child: TextApp(
+                    text: context.translate(LangKeys.chooseProducts),
+                    theme: context.textStyle.copyWith(
+                      color: context.color.textColor,
+                      fontWeight: FontWeightHelper.bold,
+                      fontSize: 20.sp,
+                    ),
+                  ),
+                ),
+                CustomFadeInLeft(
+                  duration: 800,
+                  child: CustomLinearButton(
+                    onPressed: () {},
+                    child: Center(child: SvgPicture.asset(AppImages.search)),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
       ),
     );
   }
