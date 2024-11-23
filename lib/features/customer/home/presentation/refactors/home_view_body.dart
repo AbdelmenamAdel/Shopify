4a250438109d112cf35eaea54a97bd7aaaa6fa5c
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-23 23:42:00 +0200

    add banners successfuly

--- lib/features/customer/home/presentation/refactors/home_view_body.dart
+++ lib/features/customer/home/presentation/refactors/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopify/features/customer/home/presentation/widgets/banners/banner_sliders.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({required this.scrollController, super.key});
@@ -10,16 +11,19 @@ class HomeViewBody extends StatelessWidget {
     return RefreshIndicator(
       onRefresh: () async {
         // Simulate a refresh action
-        await Future.delayed(const Duration(seconds: 1));
+        await Future<void>.delayed(const Duration(seconds: 1)); //+
       },
       child: CustomScrollView(
         controller: scrollController,
         slivers: [
           // Static header
-          SliverToBoxAdapter(
-            child: Container(
-              height: 200,
-              color: Colors.red,
+          const SliverToBoxAdapter(
+            child: BannerSliders(
+              bannersList: [
+                'https://img.freepik.com/premium-vector/cyber-monday-sale-neon-banner-design-vector-illustration-shopping-promotion_520826-2162.jpg',
+                'https://img.freepik.com/free-vector/flat-landing-page-template-cyber-monday-sale_23-2150903085.jpg',
+                'https://img.freepik.com/premium-vector/social-media-banner-social-media-template_607096-58.jpg?w=900',
+              ],
             ),
           ),
           // List with separators
