331298f01bc1e9fd9dff51e325815991f5506807
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-25 21:14:19 +0200

    get products bloc

---
+++ lib/features/customer/home/presentation/managers/get_all_products/get_all_products_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shopify/features/admin/products/data/models/get_all_products_response.dart';
+import 'package:shopify/features/customer/home/data/repo/home_repo.dart';
+
+part 'get_all_products_event.dart';
+part 'get_all_products_state.dart';
+part 'get_all_products_bloc.freezed.dart';
+
+class GetAllProductsBloc
+    extends Bloc<GetAllProductsEvent, GetAllProductsState> {
+  GetAllProductsBloc(this._repo) : super(const GetAllProductsState.loading()) {
+    on<GetAllProductsEvent>(_getAllProducts);
+  }
+  final HomeRepo _repo;
+  FutureOr<void> _getAllProducts(
+    GetAllProductsEvent event,
+    Emitter<GetAllProductsState> emit,
+  ) async {
+    emit(const GetAllProductsState.loading());
+    final result = await _repo.getAllProducts();
+    result.when(
+      success: (products) {
+        if (products.data.productsList.isEmpty) {
+          emit(const GetAllProductsState.empty());
+        } else {
+          emit(
+            GetAllProductsState.success(
+              productList: products.data.productsList,
+            ),
+          );
+        }
+      },
+      failure: (error) => emit(GetAllProductsState.error(error: error)),
+    );
+  }
+}
