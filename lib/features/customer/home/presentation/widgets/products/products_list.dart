78f99f73eec730571e877900e60ef5eac6f1836d
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-25 21:16:46 +0200

    fix error and remove shrink wrap to make the app performance high

---
+++ lib/features/customer/home/presentation/widgets/products/products_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopify/core/common/widgets/custom_product_item.dart';
+import 'package:shopify/features/admin/products/data/models/get_all_products_response.dart';
+
+class ProductsList extends StatelessWidget {
+  const ProductsList({
+    required this.productList,
+    super.key,
+  });
+
+  final List<GetAllProductsModel> productList;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 15.w),
+      sliver: SliverGrid.builder(
+        // padding: EdgeInsets.zero,
+        // padding: EdgeInsets.symmetric(horizontal: 15.w),
+        itemCount: productList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, //Number of cloums
+          crossAxisSpacing: 8, // Spacing between colums
+          mainAxisSpacing: 15, //Spacing between rows
+          childAspectRatio: 165 / 250,
+        ),
+        itemBuilder: (context, index) {
+          return CustomProductItem(
+            price: productList[index].price ?? 0,
+            categoryName: productList[index].category!.name ?? '',
+            title: productList[index].title ?? '',
+            imageUrl: productList[index].images!.first,
+            productId: int.parse(productList[index].id ?? '0'),
+          );
+        },
+      ),
+    );
+  }
+}
