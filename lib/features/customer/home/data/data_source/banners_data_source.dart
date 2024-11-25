d6e71a179028f6cbad40236edc17188d5631a84b
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-25 18:08:08 +0200

    banner datasource

--- lib/features/customer/home/data/data_source/banners_data_source.dart
+++ lib/features/customer/home/data/data_source/banners_data_source.dart
@@ -1,5 +1,9 @@
+import 'package:shopify/core/services/graphql/admin/categories_queries.dart';
+import 'package:shopify/core/services/graphql/admin/products_queries.dart';
 import 'package:shopify/core/services/graphql/api_service.dart';
 import 'package:shopify/core/services/graphql/customer/home_queries.dart';
+import 'package:shopify/features/admin/categories/data/models/get_all_categories_reponse.dart';
+import 'package:shopify/features/admin/products/data/models/get_all_products_response.dart';
 import 'package:shopify/features/customer/home/data/models/banners_response.dart';
 
 class BannersDataSource {
@@ -7,6 +11,20 @@ class BannersDataSource {
   final ApiService _api;
   Future<BannersResponse> getBanners() async {
     final response = await _api.getBanners(HomeQueries().getBannersMapQuery());
+    return response;
+  }
+
+  Future<CategoriesGetAllResponse> getAllCategories() async {
+    final response = await _api.getAllCategories(
+      CategoriesQueries().getAllCategoriesMapQuery(),
+    );
+    return response;
+  }
+
+  Future<GetAllProductsResponse> getAllProducts() async {
+    final response = await _api.getAllProduct(
+      ProductsQueries().getAllProductsMapQuery(),
+    );
     return response;
   }
 }
