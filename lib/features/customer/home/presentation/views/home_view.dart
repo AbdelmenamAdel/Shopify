80bab15ce2683aa87c0b2ca866511be9a4478a92
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-25 21:15:24 +0200

    donnnnnne

--- lib/features/customer/home/presentation/views/home_view.dart
+++ lib/features/customer/home/presentation/views/home_view.dart
@@ -4,6 +4,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopify/core/common/animations/animate_do.dart';
 import 'package:shopify/core/dependancy_injection/injection_container.dart';
 import 'package:shopify/core/extensions/context_extension.dart';
+import 'package:shopify/features/customer/home/presentation/managers/get_all_categories/get_all_categories_bloc.dart';
+import 'package:shopify/features/customer/home/presentation/managers/get_all_products/get_all_products_bloc.dart';
 import 'package:shopify/features/customer/home/presentation/managers/get_banners/get_banners_bloc.dart';
 import 'package:shopify/features/customer/home/presentation/refactors/home_view_body.dart';
 
@@ -25,8 +27,8 @@ class _HomeViewState extends State<HomeView> {
   void scrollUp() {
     scrollController.animateTo(
       0,
-      duration: const Duration(seconds: 1),
-      curve: Curves.bounceInOut,
+      duration: const Duration(milliseconds: 800),
+      curve: Curves.easeInOut,
     );
   }
 
@@ -38,6 +40,18 @@ class _HomeViewState extends State<HomeView> {
           create: (context) => sl<GetBannersBloc>()
             ..add(
               const GetBannersEvent.getBanners(),
+            ),
+        ),
+        BlocProvider(
+          create: (context) => sl<GetAllCategoriesBloc>()
+            ..add(
+              const GetAllCategoriesEvent.getAllCategories(),
+            ),
+        ),
+        BlocProvider(
+          create: (context) => sl<GetAllProductsBloc>()
+            ..add(
+              const GetAllProductsEvent.getAllProducts(),
             ),
         ),
       ],
