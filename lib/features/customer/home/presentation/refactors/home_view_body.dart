b3f3217615bfefd9652686d42c15f419c6d19975
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-25 21:15:18 +0200

    home view is done

--- lib/features/customer/home/presentation/refactors/home_view_body.dart
+++ lib/features/customer/home/presentation/refactors/home_view_body.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shopify/core/common/loading/loading_shimmer.dart';
+import 'package:shopify/features/customer/home/presentation/managers/get_all_categories/get_all_categories_bloc.dart';
+import 'package:shopify/features/customer/home/presentation/managers/get_all_products/get_all_products_bloc.dart';
 import 'package:shopify/features/customer/home/presentation/managers/get_banners/get_banners_bloc.dart';
 import 'package:shopify/features/customer/home/presentation/widgets/banners/banner_sliders.dart';
+import 'package:shopify/features/customer/home/presentation/widgets/categories/categories_list.dart';
+import 'package:shopify/features/customer/home/presentation/widgets/categories/categories_shimmer.dart';
+import 'package:shopify/features/customer/home/presentation/widgets/products/products_list.dart';
+import 'package:shopify/features/customer/home/presentation/widgets/products/products_shimmer.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({required this.scrollController, super.key});
@@ -14,8 +20,13 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return RefreshIndicator(
       onRefresh: () async {
-        // Simulate a refresh action
-        await Future<void>.delayed(const Duration(seconds: 1)); //+
+        context.read<GetBannersBloc>().add(const GetBannersEvent.getBanners());
+        context
+            .read<GetAllCategoriesBloc>()
+            .add(const GetAllCategoriesEvent.getAllCategories());
+        context
+            .read<GetAllProductsBloc>()
+            .add(const GetAllProductsEvent.getAllProducts());
       },
       child: CustomScrollView(
         controller: scrollController,
@@ -40,36 +51,122 @@ class HomeViewBody extends StatelessWidget {
                     );
                   },
                   empty: () {
-                    return const SizedBox.shrink();
+                    return Center(
+                      child: Text(
+                        'No data available',
+                        style: TextStyle(fontSize: 16.sp, color: Colors.grey),
+                      ),
+                    );
                   },
                   error: (error) {
-                    return Text('Error: $error');
+                    return Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.error, color: Colors.red, size: 48),
+                          Text(
+                            'An error occurred:\n$error',
+                            textAlign: TextAlign.center,
+                            style:
+                                TextStyle(color: Colors.red, fontSize: 16.sp),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Trigger retry logic
+                            },
+                            child: const Text('Retry'),
+                          ),
+                        ],
+                      ),
+                    );
                   },
                 );
               },
             ),
           ),
-          // List with separators
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) {
-                // Alternate between item and separator
-                if (index.isOdd) {
-                  return const SizedBox(height: 10); // Separator
-                }
-                final itemIndex = index ~/ 2; // Actual item index
-                return Container(
-                  height: 50,
-                  width: double.infinity,
-                  color: Colors.red,
-                  child: Center(child: Text('Item $itemIndex')),
+          SliverToBoxAdapter(
+            child: BlocBuilder<GetAllCategoriesBloc, GetAllCategoriesState>(
+              builder: (context, state) {
+                return state.when(
+                  success: (categoriesList) {
+                    return CategoriesList(
+                      categoreisList: categoriesList,
+                    );
+                  },
+                  loading: () => const CategoriesShimmer(),
+                  empty: () {
+                    return Center(
+                      child: Text(
+                        'No data available',
+                        style: TextStyle(fontSize: 16.sp, color: Colors.grey),
+                      ),
+                    );
+                  },
+                  error: (error) {
+                    return Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.error, color: Colors.red, size: 48),
+                          Text(
+                            'An error occurred:\n$error',
+                            textAlign: TextAlign.center,
+                            style:
+                                TextStyle(color: Colors.red, fontSize: 16.sp),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              // Trigger retry logic
+                            },
+                            child: const Text('Retry'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 );
               },
-              childCount: 50 * 2 + 1, // Items + separators
             ),
+          ),
+          BlocBuilder<GetAllProductsBloc, GetAllProductsState>(
+            builder: (context, state) {
+              return state.when(
+                success: (productsList) {
+                  return ProductsList(
+                    productList: productsList,
+                  );
+                },
+                loading: () =>
+                    const SliverToBoxAdapter(child: ProductShimmer()),
+                empty: () {
+                  return const SliverToBoxAdapter(child: SizedBox.shrink());
+                },
+                error: (error) {
+                  return SliverToBoxAdapter(child: Text('Error: $error'));
+                },
+              );
+            },
           ),
         ],
       ),
     );
   }
 }
+//  SliverList(
+//             delegate: SliverChildBuilderDelegate(
+//               (context, index) {
+//                 // Alternate between item and separator
+//                 if (index.isOdd) {
+//                   return const SizedBox(height: 10); // Separator
+//                 }
+//                 final itemIndex = index ~/ 2; // Actual item index
+//                 return Container(
+//                   height: 50,
+//                   width: double.infinity,
+//                   color: Colors.red,
+//                   child: Center(child: Text('Item $itemIndex')),
+//                 );
+//               },
+//               childCount: 50 * 2 + 1, // Items + separators
+//             ),
+//           ),
