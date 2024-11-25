9fda1ae07b5080874236704bcbe8c9818e2d293f
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-25 18:07:48 +0200

    banners repo

--- lib/features/customer/home/data/repo/banners_repo.dart
+++ lib/features/customer/home/data/repo/banners_repo.dart
@@ -1,13 +1,33 @@
 import 'package:shopify/core/services/graphql/api_result.dart';
+import 'package:shopify/features/admin/categories/data/models/get_all_categories_reponse.dart';
+import 'package:shopify/features/admin/products/data/models/get_all_products_response.dart';
 import 'package:shopify/features/customer/home/data/data_source/banners_data_source.dart';
 import 'package:shopify/features/customer/home/data/models/banners_response.dart';
 
-class BannersRepo {
-  BannersRepo(this._dataSource);
+class HomeRepo {
+  HomeRepo(this._dataSource);
   final BannersDataSource _dataSource;
   Future<ApiResult<BannersResponse>> getBanners() async {
     try {
       final response = await _dataSource.getBanners();
+      return ApiResult.success(response);
+    } on Exception catch (e) {
+      return ApiResult.failure(e.toString());
+    }
+  }
+
+  Future<ApiResult<GetAllProductsResponse>> getAllProducts() async {
+    try {
+      final response = await _dataSource.getAllProducts();
+      return ApiResult.success(response);
+    } on Exception catch (e) {
+      return ApiResult.failure(e.toString());
+    }
+  }
+
+  Future<ApiResult<CategoriesGetAllResponse>> getAllCategories() async {
+    try {
+      final response = await _dataSource.getAllCategories();
       return ApiResult.success(response);
     } on Exception catch (e) {
       return ApiResult.failure(e.toString());
