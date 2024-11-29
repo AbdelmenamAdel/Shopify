3c0909d2257849ed5f657ede95778cd6156353e5
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-29 23:06:16 +0200

    model for search

---
+++ lib/features/customer/search/data/models/search_request_body.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'search_request_body.g.dart';
+
+@JsonSerializable()
+class SearchRequestBody {
+  SearchRequestBody({
+    required this.searchName,
+    required this.priceMin,
+    required this.priceMax,
+  });
+
+  Map<String, dynamic> toJson() => _$SearchRequestBodyToJson(this);
+
+  final String? searchName;
+  final int? priceMin;
+  final int? priceMax;
+}
