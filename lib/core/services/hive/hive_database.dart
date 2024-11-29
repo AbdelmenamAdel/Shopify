9c864aa2028bc7a4aa0a92d3c34939e1b03719d3
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-11-30 00:50:30 +0200

    add favorites to the local db

--- lib/core/services/hive/hive_database.dart
+++ lib/core/services/hive/hive_database.dart
@@ -1,18 +1,24 @@
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shopify/features/admin/notifications/data/models/push_notification_model.dart';
+import 'package:shopify/features/customer/favorites/data/models/favorites_model.dart';
 
 class HiveDatabase {
   factory HiveDatabase() => _instance;
   HiveDatabase._();
   static final HiveDatabase _instance = HiveDatabase._();
   Box<PushNotificationModel>? notificationBox;
+  Box<FavoritesModel>? favoritesBox;
   Future<void> init() async {
     await Hive.initFlutter();
 
-    Hive.registerAdapter(PushNotificationModelAdapter());
+    Hive
+      ..registerAdapter(PushNotificationModelAdapter())
+      ..registerAdapter(FavoritesModelAdapter());
 
     notificationBox =
         await Hive.openBox<PushNotificationModel>('notifications_box');
+
+    favoritesBox = await Hive.openBox<FavoritesModel>('favorites_box');
   }
 
   Future<void> clearAllBox() async {
