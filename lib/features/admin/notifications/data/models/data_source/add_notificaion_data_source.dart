30d40c23cbc10cb8b3226f8990e05e7cbe745871
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-12-01 19:09:32 +0200

    notifiacation datasource

--- lib/features/admin/notifications/data/models/data_source/add_notificaion_data_source.dart
+++ lib/features/admin/notifications/data/models/data_source/add_notificaion_data_source.dart
@@ -1,5 +1,10 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shopify/core/extensions/string_exetension.dart';
 import 'package:shopify/core/services/push_notification/firebase_cloud_messaging.dart';
+import 'package:shopify/core/utils/app_strings.dart';
+import 'package:shopify/core/utils/app_values.dart';
+import 'package:shopify/features/customer/notifications/data/models/notification_model.dart';
 // import 'package:shopify/core/utils/app_values.dart';
 
 class AddNotificationDataSource {
@@ -19,33 +24,33 @@ class AddNotificationDataSource {
 
   // add Notifications to all users in firebase data base
 
-  // Future<void> addNotificationsToAllUsersFirebase({
-  //   required String body,
-  //   required String title,
-  //   required int productId,
-  // }) async {
-  //   final notificationId = AppValues.randomStringId;
-  //   const createAt = '';
+  Future<void> addNotificationsToAllUsersFirebase({
+    required String body,
+    required String title,
+    required int productId,
+  }) async {
+    final notificationId = AppValues.randomStringId;
+    const createAt = '';
 
-  //   final users = FirebaseFirestore.instance.collection(usersCollection);
+    final users = FirebaseFirestore.instance.collection(usersCollection);
 
-  //   final usersGet = await users.get();
+    final usersGet = await users.get();
 
-  //   for (final doc in usersGet.docs) {
-  //     await users
-  //         .doc(doc.id)
-  //         .collection(notificationCollection)
-  //         .doc(notificationId)
-  //         .set(
-  //           NotificationModel(
-  //             body: body,
-  //             title: title,
-  //             productId: productId,
-  //             isSeen: false,
-  //             createdAt: createAt.convertDataFormate(),
-  //             notificationId: notificationId,
-  //           ).toJson(),
-  //         );
-  //   }
-  // }
+    for (final doc in usersGet.docs) {
+      await users
+          .doc(doc.id)
+          .collection(notificationCollection)
+          .doc(notificationId)
+          .set(
+            NotificationModel(
+              body: body,
+              title: title,
+              productId: productId,
+              isSeen: false,
+              createdAt: createAt.convertDataFormate(),
+              notificationId: notificationId,
+            ).toJson(),
+          );
+    }
+  }
 }
