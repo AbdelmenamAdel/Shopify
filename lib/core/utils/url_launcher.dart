9e5dba2cea937125a6e4d7ae14334745001506bb
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-12-01 19:09:19 +0200

    url launcher

---
+++ lib/core/utils/url_launcher.dart
@@ -0,0 +1,16 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> urlLauncher(BuildContext context, String url) async {
+  final Url = Uri.parse(url);
+  if (!await launchUrl(
+    Url,
+    mode: LaunchMode.externalNonBrowserApplication,
+  )) {
+    log('Could not launch $url');
+  }
+  if (await canLaunchUrl(Url)) {
+    await launchUrl(Url);
+  } else {}
+}
